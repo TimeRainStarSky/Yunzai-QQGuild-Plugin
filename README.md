ea438e40714526f4752539f3ff75e8a5bffee45d
🌌 <Time.Rain.Star.Sky@Gmail.com>
2023-05-08 15:25:33 +0800

    测试版发布

---
+++ README.md
@@ -0,0 +1,31 @@
+<div align="center">
+
+# TRSS-Yunzai QQGuild Plugin
+
+TRSS-Yunzai QQ频道 Bot 适配器 插件
+
+[![访问量](https://visitor-badge.glitch.me/badge?page_id=TimeRainStarSky.Yunzai-QQGuild-Plugin&right_color=red&left_text=访%20问%20量)](https://github.com/TimeRainStarSky/Yunzai-QQGuild-Plugin)
+[![Stars](https://img.shields.io/github/stars/TimeRainStarSky/Yunzai-QQGuild-Plugin?color=yellow&label=收藏)](../../stargazers)
+[![Downloads](https://img.shields.io/github/downloads/TimeRainStarSky/Yunzai-QQGuild-Plugin/total?color=blue&label=下载)](../../archive/main.tar.gz)
+[![Releases](https://img.shields.io/github/v/release/TimeRainStarSky/Yunzai-QQGuild-Plugin?color=green&label=发行版)](../../releases/latest)
+
+[![访问量](https://profile-counter.glitch.me/TimeRainStarSky-Yunzai-QQGuild-Plugin/count.svg)](https://github.com/TimeRainStarSky/Yunzai-QQGuild-Plugin)
+
+</div>
+
+## 安装教程
+
+1. 准备：[TRSS-Yunzai](../../../Yunzai)
+
+2. 自动安装：`#安装QQGuild-Plugin`
+- 手动安装：[GitHub](https://github.com/TimeRainStarSky/Yunzai-QQGuild-Plugin) 或 [Gitee](https://gitee.com/TimeRainStarSky/Yunzai-QQGuild-Plugin)
+
+```
+git clone --depth 1 https://gitee.com/TimeRainStarSky/Yunzai-QQGuild-Plugin plugins/QQGuild-Plugin
+pnpm i
+```
+
+## 使用教程
+
+- #QQ频道账号
+- #QQ频道设置 + `是否沙箱:是否私域:AppID:Token`（是1 否0）
---
+++ index.js
@@ -0,0 +1,303 @@
+logger.info(logger.yellow("- 正在加载 QQ频道 插件"))
+
+import { config, configSave } from "./Model/config.js"
+import { createOpenAPI, createWebsocket } from "qq-guild-bot"
+import { FormData, Blob } from "formdata-node"
+
+const adapter = new class QQGuildAdapter {
+  sendImage(data, send, file) {
+    logger.info(`${logger.blue(`[${data.self_id}]`)} 发送图片：${file.replace(/^base64:\/\/.*/, "base64://...")}`)
+    if (file.match(/^base64:\/\//)) {
+      const formdata = new FormData()
+      if (data.message_id)
+        formdata.set("msg_id", data.message_id)
+      formdata.set("file_image", new Blob([Buffer.from(file.replace(/^base64:\/\//, ""), "base64")]))
+      return send(formdata)
+    } else {
+      return send({ image: file, msg_id: data.message_id })
+    }
+  }
+
+  async sendMsg(data, send, msg) {
+    if (!Array.isArray(msg))
+      msg = [msg]
+    let message = ""
+    for (let i of msg) {
+      if (typeof i != "object")
+        i = { type: "text", data: { text: i } }
+      else if (!i.data)
+        i = { type: i.type, data: { ...i, type: undefined } }
+      switch (i.type) {
+        case "text":
+          message += i.data.text
+          break
+        case "image":
+          await this.sendImage(data, send, i.data.file)
+          break
+        case "face":
+          message += `<emoji:${i.data.id}>`
+          break
+        case "reply":
+          data.message_id = i.data.id
+          break
+        case "at":
+          if (i.data.qq == "all")
+            message += "@everyone"
+          else
+            message += `<@${i.data.qq}>`
+          break
+        default:
+          message += JSON.stringify(i)
+      }
+    }
+    if (message)
+      return send({ content: message, msg_id: data.message_id })
+  }
+
+  sendFriendMsg(data, msg) {
+    data.isDirect = true
+    return this.sendMsg(data, msg => {
+      logger.info(`${logger.blue(`[${data.self_id}]`)} 发送好友消息：[${data.guild_id}] ${JSON.stringify(msg)}`)
+      return data.bot.api.directMessageApi.postDirectMessage(data.guild_id, msg)
+    }, msg)
+  }
+
+  sendGroupMsg(data, msg) {
+    data.isDirect = false
+    return this.sendMsg(data, msg => {
+      logger.info(`${logger.blue(`[${data.self_id}]`)} 发送群消息：[${data.channel_id}] ${JSON.stringify(msg)}`)
+      return data.bot.api.messageApi.postMessage(data.channel_id, msg)
+    }, msg)
+  }
+
+  recallMsg(data, message_id, hide) {
+    return data.bot.api.messageApi.deleteMessage(data.channel_id, message_id, hide)
+  }
+
+  async makeForwardMsg(send, msg) {
+    const messages = []
+    for (const i of msg)
+      messages.push(await send(i.message))
+    messages.data = "消息"
+    return messages
+  }
+
+  makeMessage(data) {
+    data = {
+      ...data,
+      ...data.msg,
+      msg: undefined,
+    }
+    data.user_id = data.author.id
+    data.sender = {
+      nickname: data.author.username
+    }
+    data.group_id = `${data.guild_id}-${data.channel_id}`
+    data.post_type = "message"
+    data.message_id = data.id
+
+    data.message = []
+    data.raw_message = ""
+    if (data.content) {
+      const at = data.content.match(/^<@![0-9]+>/)
+      if (at) {
+        const qq = at[0].replace(/^<@!([0-9]+)>/, "$1")
+        data.message.push({ type: "at", qq })
+        data.raw_message += `[提及：${qq}]`
+        data.content = data.content.replace(/^<@![0-9]+>/, "").trim()
+      }
+      data.message.push({ type: "text", text: data.content })
+      data.raw_message += data.content
+    }
+
+    data.friend = data.bot.pickFriend(data.user_id)
+    return data
+  }
+
+  makeFriendMessage(data) {
+    data = this.makeMessage(data)
+    data.message_type = "private"
+
+    logger.info(`${logger.blue(`[${data.self_id}]`)} 好友消息：[${data.group_id}, ${data.sender.nickname}(${data.user_id})] ${data.raw_message}`)
+    data.reply = msg => this.sendFriendMsg(data, msg)
+
+    Bot.emit(`${data.post_type}.${data.message_type}`, data)
+    Bot.emit(`${data.post_type}`, data)
+  }
+
+  makeGroupMessage(data) {
+    data = this.makeMessage(data)
+    data.message_type = "group"
+
+    logger.info(`${logger.blue(`[${data.self_id}]`)} 群消息：[${data.group_id}, ${data.sender.nickname}(${data.user_id})] ${data.raw_message}`)
+    data.group = data.bot.pickGroup(data.group_id)
+    data.member = data.group.pickMember(data.user_id)
+    data.reply = msg => this.sendGroupMsg(data, msg)
+
+    Bot.emit(`${data.post_type}.${data.message_type}`, data)
+    Bot.emit(`${data.post_type}`, data)
+  }
+
+  message(bot, data) {
+    data.self_id = bot.uin
+    data.bot = bot
+    switch (data.eventType) {
+      case "MESSAGE_CREATE":
+        this.makeGroupMessage(data)
+        break
+      case "MESSAGE_DELETE":
+        break
+      case "DIRECT_MESSAGE_CREATE":
+        this.makeFriendMessage(data)
+        break
+      case "DIRECT_MESSAGE_DELETE":
+        break
+      case "AT_MESSAGE_CREATE":
+        this.makeGroupMessage(data)
+        break
+      case "PUBLIC_MESSAGE_DELETE":
+        break
+      default:
+    }
+  }
+
+  async connect(token) {
+    token = token.split(":")
+
+    const intents = [
+      "GUILDS",
+      "GUILD_MEMBERS",
+      "GUILD_MESSAGE_REACTIONS",
+      "DIRECT_MESSAGE",
+      "OPEN_FORUMS_EVENT",
+      "INTERACTION",
+      "MESSAGE_AUDIT",
+      "PUBLIC_GUILD_MESSAGES",
+    ]
+    if (Number(token[1]))
+      intents.push("GUILD_MESSAGES", "FORUMS_EVENT")
+
+    const bot = {
+      appID: token[2],
+      token: token[3],
+      intents,
+      sandbox: Boolean(Number(token[0])),
+    }
+    bot.api = createOpenAPI(bot)
+    bot.ws = createWebsocket(bot)
+    bot.ws.on("ERROR", logger.error)
+
+    bot.info = {
+      ...(await new Promise(resolve => bot.ws.once("READY", data => resolve(data))))?.msg?.user,
+      ...(await bot.api.meApi.me()).data,
+    }
+    if (!bot.info.id) {
+      logger.error(`${logger.blue(`[${token}]`)} QQ频道Bot 连接失败`)
+      return false
+    }
+
+    const id = bot.info.id
+    Bot[id] = bot
+    Bot[id].uin = id
+    Bot[id].nickname = Bot[id].info.username
+    Bot[id].avatar = Bot[id].info.avatar
+    Bot[id].stat = { start_time: Date.now() / 1000 }
+    Bot[id].fl = new Map()
+    Bot[id].gl = new Map()
+
+    Bot[id].pickFriend = user_id => {
+      const i = { self_id: id, bot: Bot[id], user_id }
+      return {
+        sendMsg: msg => this.sendFriendMsg(i, msg),
+        recallMsg: (message_id, hide) => this.recallMsg(i, message_id, hide),
+        makeForwardMsg: msg => this.makeForwardMsg(msg => this.sendFriendMsg(i, msg), msg),
+      }
+    }
+    Bot[id].pickUser = Bot[id].pickFriend
+
+    Bot[id].pickMember = (group_id, user_id) => {
+      group_id = group_id.split("-")
+      const i = { self_id: id, bot: Bot[id], guild_id: group_id[0], channel_id: group_id[1], user_id }
+      return {
+        ...Bot[id].pickFriend(user_id),
+      }
+    }
+
+    Bot[id].pickGroup = group_id => {
+      group_id = group_id.split("-")
+      const i = { self_id: id, bot: Bot[id], guild_id: group_id[0], channel_id: group_id[1] }
+      return {
+        sendMsg: msg => this.sendGroupMsg(i, msg),
+        recallMsg: (message_id, hide) => this.recallMsg(i, message_id, hide),
+        makeForwardMsg: msg => this.makeForwardMsg(msg => this.sendGroupMsg(i, msg), msg),
+        pickMember: user_id => i.bot.pickMember(`${i.guild_id}-${i.channel_id}`, user_id),
+      }
+    }
+
+    if (Array.isArray(Bot.uin)) {
+      if (!Bot.uin.includes(id))
+        Bot.uin.push(id)
+    } else {
+      Bot.uin = [id]
+    }
+
+    Bot[id].ws.on("GUILD_MESSAGES", data => this.message(Bot[id], data))
+    Bot[id].ws.on("DIRECT_MESSAGE", data => this.message(Bot[id], data))
+    Bot[id].ws.on("PUBLIC_GUILD_MESSAGES", data => this.message(Bot[id], data))
+
+    logger.mark(`${logger.blue(`[${id}]`)} QQ频道Bot 已连接`)
+    Bot.emit(`connect.${id}`, Bot[id])
+    Bot.emit(`connect`, Bot[id])
+    return true
+  }
+}
+
+Bot.once("online", async () => {
+  for (const token of config.token)
+    await adapter.connect(token)
+})
+
+export class QQGuild extends plugin {
+  constructor() {
+    super({
+      name: "QQ频道",
+      dsc: "QQ频道",
+      event: "message",
+      rule: [
+        {
+          reg: "^#[Qq]+(频道|[Gg]uild)账号$",
+          fnc: "List",
+          permission: "master"
+        },
+        {
+          reg: "^#[Qq]+(频道|[Gg]uild)设置[01]:[01]:[0-9]+:.*$",
+          fnc: "Token",
+          permission: "master"
+        }
+      ]
+    })
+  }
+
+  async List() {
+    await this.reply(`共${config.token.length}个账号：\n${config.token.join("\n")}`, true)
+  }
+
+  async Token() {
+    const token = this.e.msg.replace(/^#[Qq]+(频道|[Gg]uild)设置/, "").trim()
+    if (config.token.includes(token)) {
+      config.token = config.token.filter(item => item != token)
+      await this.reply(`账号已删除，重启后生效，共${config.token.length}个账号`, true)
+    } else {
+      if (await adapter.connect(token)) {
+        config.token.push(token)
+        await this.reply(`账号已连接，共${config.token.length}个账号`, true)
+      } else {
+        await this.reply(`账号连接失败`, true)
+        return false
+      }
+    }
+    configSave(config)
+  }
+}
+
+logger.info(logger.green("- QQ频道 插件 加载完成"))
